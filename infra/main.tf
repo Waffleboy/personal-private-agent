1aaa326ba611484cfc94822ca9befbf60feb0bff
Thiru <Waffleboy@users.noreply.github.com>
2026-06-27 17:19:15 +0800

    feat: HTTP API Gateway with POST /webhook route

--- infra/main.tf
+++ infra/main.tf
@@ -145,3 +145,35 @@ resource "aws_lambda_function" "bot" {
     }
   }
 }
+
+resource "aws_apigatewayv2_api" "http" {
+  name          = var.name_prefix
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.bot.arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "webhook" {
+  api_id    = aws_apigatewayv2_api.http.id
+  route_key = "POST /webhook"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.bot.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http.execution_arn}/*/*"
+}
